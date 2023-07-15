40e51d4fcae18c34a78d0f4bde6861f63124ebe1
AtharvaSatav <132169829+AtharvaSatav@users.noreply.github.com>
2023-07-15 06:36:04 +0530

    obj_detect.py

--- obj_detect.py
+++ obj_detect.py
@@ -3,10 +3,10 @@ import cv2
 import argparse
 import os
 
-api_url = "http://localhost:9900/obj_detect"
+api_url = "http://localhost:9900/v1/detectobjects"
+image_path = os.path.join("obj_detect_example", "car.jpg")
 
-
-image = cv2.imread(car.jpg)
+image = cv2.imread(image_path)
 retval,image_file = cv2.imencode('.jpg', image)
 if retval:
     encoded_image = image_file.tobytes()
@@ -19,13 +19,16 @@ response = requests.post(api_url, encoded_image)
 if response.status_code == 200:
     response_data = response.json()
 
-    objects = response_data['objects']
-
+    print(response_data)
+    objects = response_data["result"]["objects"]
     for obj in objects:
-        label = obj['label']
-        confidence = obj['confidence']
-        x, y, w, h = obj['bounding_box']
-        print(f"Object: {label}, Confidence: {confidence}, Bounding Box: ({x}, {y}, {w}, {h})")
+      if obj["boundingBox"]["width"] > 0 and obj["boundingBox"]["height"] > 0:
+        left = int(obj["boundingBox"]["top"])
+        top = int(obj["boundingBox"]["left"])
+        width = int(obj["boundingBox"]["width"])
+        height = int(obj["boundingBox"]["height"])
+        cv2.rectangle(image, (left, top), (left + width, top + height), (0, 255, 0), 2)
+
 
     cv2.imshow("detected object", image)
     print("Press any key to continue...")
